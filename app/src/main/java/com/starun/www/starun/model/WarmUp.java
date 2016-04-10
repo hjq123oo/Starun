6d5e4829b5a3043f535f9ce5f357b1d6f46d44a7
lxn <13301040@bjtu.edu.cn>
2016-04-10 16:15:05 +0800

    model and pview

---
+++ app/src/main/java/com/starun/www/starun/model/WarmUp.java
@@ -0,0 +1,10 @@
+package com.starun.www.starun.model;
+
+/**
+ * Created by xiaoxue on 2016/4/10.
+ */
+public class WarmUp extends Exercise{
+    public int START=100;
+    public int PAUSE=101;
+    public int STOP=102;
+}
