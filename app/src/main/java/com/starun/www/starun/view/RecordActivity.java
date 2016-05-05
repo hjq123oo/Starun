b1a04a9f103ec238d28212280d5818e07361cb41
tpian928 <tpian928@icloud.com>
2016-05-05 08:35:35 +0800

    ok

---
+++ app/src/main/java/com/starun/www/starun/view/RecordActivity.java
@@ -0,0 +1,15 @@
+package com.starun.www.starun.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.starun.www.starun.R;
+
+public class RecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+    }
+}
