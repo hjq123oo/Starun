8bfaf877e98e39a67e828b1365cda1166c9b7845
tpian928 <tpian928@icloud.com>
2016-05-05 10:37:37 +0800

    sure

--- app/src/main/java/com/starun/www/starun/adapter/RecordListAdapter.java
+++ app/src/main/java/com/starun/www/starun/adapter/RecordListAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.starun.www.starun.R;
 import com.starun.www.starun.model.data.RunRecord;
@@ -40,10 +42,25 @@ public class RecordListAdapter extends BaseAdapter {
         return position;
     }
 
+    public class Holder
+    {
+        ImageView icon1;
+        ImageView icon2;
+        TextView date_tv;
+        TextView distance_tv;
+        TextView spend_time_tv;
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        Holder holder = new Holder();
         View rootView = inflater.inflate(R.layout.record_listview_item, null);
-
+        holder.date_tv = (TextView)rootView.findViewById(R.id.date_tv);
+        holder.distance_tv = (TextView)rootView.findViewById(R.id.distance_tv);
+        holder.spend_time_tv = (TextView)rootView.findViewById(R.id.spend_time_tv);
+        holder.icon1 = (ImageView)rootView.findViewById(R.id.icon1);
+        holder.icon2 = (ImageView)rootView.findViewById(R.id.icon2);
+        holder.date_tv.setText(records.get(position).getStartTime()+"");
 
         return rootView;
     }
