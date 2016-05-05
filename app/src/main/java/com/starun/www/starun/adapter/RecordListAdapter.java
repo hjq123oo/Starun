081636aafb24283043d9800e8f9c46fdeb47eb86
tpian928 <tpian928@icloud.com>
2016-05-05 10:15:57 +0800

    save...

--- app/src/main/java/com/starun/www/starun/adapter/RecordListAdapter.java
+++ app/src/main/java/com/starun/www/starun/adapter/RecordListAdapter.java
@@ -6,6 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.starun.www.starun.R;
+import com.starun.www.starun.model.data.RunRecord;
+
+import java.util.ArrayList;
+
 /**
  * Created by TPIAN on 16/5/5.
  */
@@ -13,28 +18,33 @@ public class RecordListAdapter extends BaseAdapter {
 
     Context context;
     private static LayoutInflater inflater=null;
+    ArrayList<RunRecord> records;
 
-    public RecordListAdapter(Context context) {
+    public RecordListAdapter(Context context,ArrayList<RunRecord> records) {
         this.context = context;
+        this.records = records;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return records.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return position;
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        View rootView = inflater.inflate(R.layout.record_listview_item, null);
+
+
+        return rootView;
     }
 }
