fd8aee5acadd7a682550515481a064ff7c6665b5
hjq <hjq123oooo@163.com>
2016-05-12 11:45:01 +0800

    modify IRunPlanExecution

---
+++ app/src/main/java/com/starun/www/starun/model/data/Mp3Info.java
@@ -0,0 +1,61 @@
+package com.starun.www.starun.model.data;
+
+/**
+ * Created by hjq on 2016/5/11.
+ */
+public class Mp3Info {
+    private long id;
+    private String title;
+    private String artist;
+    private long duration;
+    private long size;
+    private String url;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
