6b8b907ec5e86afbdb7fd89fa7781bbf1ce32c7f
yearsj <yearsj111110@163.com>
2016-05-20 13:56:58 +0800

    Revert "Revert "增加部分viewadapter，还未实现""
    This reverts commit 4c0a664a43e6658bd0c29f1d765815345a0121c7.

---
+++ app/src/main/java/com/starun/www/starun/view/fragment/DailyFragment.java
@@ -0,0 +1,10 @@
+package com.starun.www.starun.view.fragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by yearsj on 2016/5/20.
+ */
+public class DailyFragment extends Fragment{
+
+}
---
+++ app/src/main/java/com/starun/www/starun/view/fragment/PlanFragment.java
@@ -0,0 +1,9 @@
+package com.starun.www.starun.view.fragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by yearsj on 2016/5/20.
+ */
+public class PlanFragment  extends Fragment {
+}
