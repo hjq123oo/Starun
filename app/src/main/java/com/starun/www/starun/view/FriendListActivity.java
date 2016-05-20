6ca18f114fa20cf1cdd232e340b4d1e0150477b6
yearsj <yearsj111110@163.com>
2016-05-20 16:13:12 +0800

    by sj

--- app/src/main/java/com/starun/www/starun/view/FriendListActivity.java
+++ app/src/main/java/com/starun/www/starun/view/FriendListActivity.java
@@ -9,7 +9,10 @@ import android.widget.Button;
 
 import com.starun.www.starun.R;
 import com.starun.www.starun.view.customview.MyFragmentAdapter;
+import com.starun.www.starun.view.fragment.DailyFragment;
+import com.starun.www.starun.view.fragment.PlanFragment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,6 +33,9 @@ public class FriendListActivity extends FragmentActivity {
 
     public void init(){
         mViewPager = (ViewPager)findViewById(R.id.fri_viewpager);
+        fragments = new ArrayList<Fragment>();
+        fragments.add(new PlanFragment());
+        fragments.add(new DailyFragment());
         mViewPager.setAdapter(new MyFragmentAdapter(this.getSupportFragmentManager(), fragments));
         mViewPager.setCurrentItem(0);
         mViewPager.addOnPageChangeListener(new MyOnPageChangeListener());
@@ -43,7 +49,6 @@ public class FriendListActivity extends FragmentActivity {
      * 内部类----viewpager监听器
      */
     public class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
-       // private int one = offset*2 +bmpW;//两个相邻页面的偏移量
         @Override
         public void onPageScrolled(int arg0, float arg1, int arg2) {
         }
@@ -54,11 +59,6 @@ public class FriendListActivity extends FragmentActivity {
         @Override
         public void onPageSelected(int arg0) {
             // TODO Auto-generated method stub
-//            Animation animation = new TranslateAnimation(1000,100,0,0);//平移动画
-//            //currIndex = arg0;
-//            animation.setFillAfter(true);//动画终止时停留在最后一帧，不然会回到没有执行前的状态
-//            animation.setDuration(200);//动画持续时间0.2秒
-//            image.startAnimation(animation);//是用ImageView来显示动画的
         }
     }
 
