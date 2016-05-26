707c8850014472a4a1c3b2eed118911f063b5651
hjq <hjq123oooo@163.com>
2016-05-26 14:42:10 +0800

    create UserActivity

---
+++ app/src/main/java/com/starun/www/starun/view/UserActivity.java
@@ -0,0 +1,15 @@
+package com.starun.www.starun.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.starun.www.starun.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
